16008fd8458de2c651b9ba950bc39d69a6759e8b
Philippine Berton <Phi.berton@hotmail.fr>
2019-04-01 12:54:51 +0200

    appointments & calendar

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,7 +6,7 @@ class PagesController < ApplicationController
 
   def calendar
     today = Date.today
-    @days_from_this_week = (today.at_beginning_of_week..today.at_end_of_week).map
+    @days_from_this_week = (today.at_beginning_of_week..today.at_end_of_week).map.first(5)
     @appointments = Appointment.where(user_id: current_user, date: today.at_beginning_of_week..today.at_end_of_week)
   end
 end
